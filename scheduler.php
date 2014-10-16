cb2c17f4ecee06927abd2065b7837827797ea201
honeysingh1 <singh178@purdue.edu>
2014-10-16 12:05:58 -0400

    Updated

--- scheduler.php
+++ scheduler.php
@@ -1,21 +1,24 @@
- <?php 
-mysqli_connect("128.46.116.11","LCCenter","LCC.team4","lcc")
-    or die ("Could not connect"); 
+ <?php
+ mysql_connect("128.46.116.11","LCCenter","LCC.team4","lcc")
+ or die("could not");
 
-mysql_select_db('lcc');
+ mysql_select_db('lcc');
+ $result = mysql_query("SELECT * FROM USERS");
+ if (!$result) {
+     echo 'Could not run query: '. mysql_error();
+     exit;
+ }
+ while($row = mysql_fetch_array($result)) {
+     echo $row['PRIMARY ID']." ".$row['FIRST']." ".$row['LAST']." ".$row['SKILL LEVEL'];
+     echo "<br>";
+ }
+ if (mysql_num_rows($result) > 0) {
+     while ($row = mysql_fetch_assoc($result)) {
+         print_r($row);
+     }
+ }
 
-$result = mysql_query("SELECT 'PRIMARY ID' FROM USERS");
-if (!$result) {
-    echo 'Could not run query: ' . mysql_error();
-    exit;
-}
-if (mysql_num_rows($result) > 0) {
-    while ($row = mysql_fetch_assoc($result)) {
-        print_r($row);
-    }
-}
+ mysql_close();
 
-
-    mysqli_close();
 
 ?>
