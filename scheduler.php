a2119201ce0e297e7045cf2eb7e01f4ebeb48aa8
Tim Vincent <vincent3@purdue.edu>
2014-10-16 11:16:12 -0400

    Added SQL connection

---
+++ scheduler.php
@@ -0,0 +1,11 @@
+ <?php echo '<p>Hello World</p>';
+$con=mysqli_connect("128.46.116.11","LCCenter","LCC.team4","lcc");
+
+// Check connection
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+else
+        echo "Connection Success!";
+?>
+~
