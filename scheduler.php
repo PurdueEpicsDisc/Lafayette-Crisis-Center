72d2c99fc2507187beab1b6297fbd8929fd41d07
Tim Vincent <vincent3@purdue.edu>
2014-10-23 11:25:10 -0400

    added Show

--- scheduler.php
+++ scheduler.php
@@ -9,21 +9,23 @@ mysql_connect("128.46.116.11","LCCenter","LCC.team4","lcc")
 or die("could not");
 
 mysql_select_db('lcc');
-$result = mysql_query("SELECT * FROM USERS");
-if (!$result) {
-    echo 'Could not run query: '. mysql_error();
-    exit;
-}
-/*while($row = mysql_fetch_array($result)) {
-    printf("%3d %3s %3s %s",$row['PRIMARY ID'],$row['FIRST'],$row['LAST'],$row['SKILL LEVEL']);
-    echo "<br>";
-}*/
-/*if (mysql_num_rows($result) > 0) {
-    while ($row = mysql_fetch_assoc($result)) {
-        print_r($row);
-    }
-}*/
 
+function show(){
+    $result = mysql_query("SELECT * FROM USERS");
+    if (!$result) {
+        echo 'Could not run query: '. mysql_error();
+        exit;
+    }
+    while($row = mysql_fetch_array($result)) {
+        echo $row['PRIMARY ID']." ".$row['FIRST']." ".$row['LAST']." ".$row['SKILL LEVEL'];
+        echo "<br>";
+    }
+    if (mysql_num_rows($result) > 0) {
+        while ($row = mysql_fetch_assoc($result)) {
+         print_r($row);
+        }
+    }
+}
 function addUser($firstName, $lastName, $skillLevel)
 {
     $sql = "INSERT INTO USERS(['FIRST'], ['LAST'], ['SKILL LEVEL'])
