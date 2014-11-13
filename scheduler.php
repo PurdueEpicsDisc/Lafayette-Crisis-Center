d853c04df2510e314f04b44fa44fadeb94e8dde4
Tim Vincent <vincent3@purdue.edu>
2014-11-13 11:56:30 -0500

    Implemented algorithm + moved selection by skill level

--- scheduler.php
+++ scheduler.php
@@ -1,43 +1,41 @@
-<form method="post">
-    <label for="volunteer">Select skill level:</label>
-    <select name="volunteer">
-        <option value="0" selected>0</option>
-        <option value="1">1</option>
-        <option value="2">2</option>
-    </select>
-    <input type="submit" />
-</form>
-
 <?php
 $link = new mysqli("128.46.116.11", "LCCenter", "LCC.team4", "lcc");
-    if (!$link) {
-        die("Connection failed: " . $mysqli->error());
-    }
-    if($_SERVER['REQUEST_METHOD']=='POST') {
-     // Open a MySQL connection
-        $sql = "SELECT FIRST,LAST FROM USERS WHERE SKILL_LEVEL=?";
-        if($stmt = $link->prepare($sql)){
-            $stmt->bind_param('i', $_POST['volunteer']);
-            $stmt->execute();
-            $stmt->bind_result($first, $last);
-            while($stmt->fetch()) {
-                printf("Volunteer: %s %s<br />", $first, $last);
+if (!$link) {
+    die("Connection failed: " . $mysqli->error());
+}
+    /**$users init to size of shifts **/
+    $users = array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+    $priorities= array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+    // Open a MySQL connection
+    $sql = "SELECT * FROM SHIFTS ";
+    if($stmt = $link->prepare($sql)){
+        $stmt->execute();
+        $stmt->bind_result($id, $userID, $shiftID , $priority);
+        while($stmt->fetch()) {
+            if($priority > $priorities[$shiftID]){
+                $users[$shiftID] = $userID;
+                $priorities[$shiftID] = $priority;
             }
-            $stmt->close();
         }
-        else {
-            ?>
-            <form method="post">
-                <label for="volunteer">Select a Volunteer:</label>
-                <select name="volunteer">
-                    <option value="0">0</option>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                </select>
-                <input type="submit" />
-            </form>
-        <?php
-        }
-     }
+        $stmt->close();
 
-?>  
+        /* Convert userID -> Last, First */
+        for($i = 0; $i < 13; $i++) {
+            if($users[$i] != 0 && $priorities[$i] != 0) {
+                $sql = "SELECT FIRST,LAST FROM USERS WHERE PRIMARY_ID=$users[$i]";
+                if($stmt = $link->prepare($sql)) {
+                    $stmt->execute();
+                    $stmt->bind_result($first, $last);
+                    while($stmt->fetch()) {
+                        $users[$i] = $last.", ".$first;
+                    }
+                        $stmt->close();
+                }
+                //echo("User: $users[$i]\n");
+            }
+        }
+        echo("Done\n");
+
+    }
+
+?>
