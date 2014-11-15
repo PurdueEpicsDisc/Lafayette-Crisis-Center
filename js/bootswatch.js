fdd89356cd6d6b6b91c027842dfe7e1a7f6eb501
Tim Vincent <vincent3@purdue.edu>
2014-11-14 20:36:49 -0500

    Cleaned up Table + removed tooltips

--- js/bootswatch.js
+++ js/bootswatch.js
@@ -1,11 +1,11 @@
 (function(){
 
-  var $button = $("<div id='source-button' class='btn btn-primary btn-xs'>&lt; &gt;</div>").click(function(){
+/*  var $button = $("<div id='source-button' class='btn btn-primary btn-xs'>&lt; &gt;</div>").click(function(){
     var html = $(this).parent().html();
     html = cleanSource(html);
     $("#source-modal pre").text(html);
     $("#source-modal").modal();
-  });
+  });*/
 
   $('.bs-component [data-toggle="popover"]').popover();
   $('.bs-component [data-toggle="tooltip"]').tooltip();
